9e474c68bb8679f277af601f0a3ed7f906b5ef35
amitai salmon <amitai1997@gmail.com>
2023-06-10 16:48:32 +0300

    add data to task service

--- app/controllers/task_controller.py
+++ app/controllers/task_controller.py
@@ -1,20 +1,21 @@
 from flask import Blueprint, jsonify, request
 from app.models.task_model import Task
 from app.repositories.task_repository import TaskRepository
+from app.services.task_services import TaskService
 
 task_bp = Blueprint('task_bp', __name__, url_prefix="/tasks")
-task_repo = TaskRepository()
+task_service = TaskService()
 
 
 @task_bp.route('/', methods=['GET'])
 def get_all_tasks():
-    tasks = task_repo.get_all_tasks()
+    tasks = task_service.get_all_tasks()
     return jsonify(tasks), 200
 
 
 @task_bp.route('/<int:task_id>', methods=['GET'])
 def get_task(task_id):
-    task = task_repo.get_task_by_id(task_id)
+    task = task_service.get_task_by_id(task_id)
     if task:
         return jsonify(task), 200
     else:
@@ -24,14 +25,14 @@ def get_task(task_id):
 @task_bp.route('/', methods=['POST'])
 def create_task():
     data = request.get_json()
-    task = task_repo.create_task(data)
+    task = task_service.create_task(data)
     return jsonify(task), 201
 
 
 @task_bp.route('/<int:task_id>', methods=['PUT'])
 def update_task(task_id):
     data = request.get_json()
-    task = task_repo.update_task(task_id, data)
+    task = task_service.update_task(task_id, data)
     if task:
         return jsonify(task), 200
     else:
@@ -40,7 +41,7 @@ def update_task(task_id):
 
 @task_bp.route('/<int:task_id>', methods=['DELETE'])
 def delete_task(task_id):
-    result = task_repo.delete_task(task_id)
+    result = task_service.delete_task(task_id)
     if result:
         return '', 204
     else:
