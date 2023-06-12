1f5071522dfadd700073b51abc5a8c40d97cc4e1
amitai salmon <amitai1997@gmail.com>
2023-06-12 13:20:56 +0300

    config.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from app.config import config
+
 
 db = SQLAlchemy()
 migrate = Migrate()
 
 
-def create_app():
+def create_app(config_name):
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:example@db:5432/task_management'
+
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
     db.init_app(app)
     migrate.init_app(app, db)
 
--- app/app.py
+++ app/app.py
@@ -1,14 +1,16 @@
-from app.controllers.project_controller import project_bp
-from app.controllers.task_controller import task_bp
+import os
 from app.models.task_model import Task
 from app.models.user_model import User
 from app.models.project_model import Project
 from app.models.project_task_model import ProjectTask
+from app.controllers.project_controller import project_bp
+from app.controllers.task_controller import task_bp
+from dotenv import load_dotenv
 
 from . import create_app
 
 
-app = create_app()
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 # Register the blueprints/controllers here
 app.register_blueprint(task_bp)
