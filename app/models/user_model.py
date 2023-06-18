7da14a7ecbf58c68181da669eeedb4c5460ef6fc
amitai salmon <amitai1997@gmail.com>
2023-06-18 14:08:19 +0300

    change user table name to users table

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -8,13 +8,12 @@ from .base_model import BaseModel
 
 @dataclass
 class User(BaseModel, db.Model):
-    __tablename__ = "user"
+    __tablename__ = "users"
     id: int
     username: str
     email: str
     first_name: str
     last_name: str
-    registered_on: str
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
@@ -22,7 +21,6 @@ class User(BaseModel, db.Model):
     password = db.Column(db.String(255), nullable=False)
     first_name = db.Column(db.String(128), nullable=False)
     last_name = db.Column(db.String(128), nullable=False)
-    registered_on = db.Column(db.DateTime, nullable=False)
 
     def __init__(self, username, email, password, first_name, last_name):
         self.username = username
@@ -31,7 +29,6 @@ class User(BaseModel, db.Model):
             'utf-8'), bcrypt.gensalt(current_app.config['BCRYPT_LOG_ROUNDS']))
         self.first_name = first_name
         self.last_name = last_name
-        self.registered_on = datetime.datetime.now()
 
     def serialize(self) -> dict:
         serialized_model = super().serialize()
