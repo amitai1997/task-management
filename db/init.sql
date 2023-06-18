063d2bb24942ccf99ad2073ea83e07d28482869a
amitai salmon <amitai1997@gmail.com>
2023-06-18 14:31:06 +0300

    delete db folder

--- db/init.sql
+++
@@ -1,9 +0,0 @@
--- Create the transactions table
-CREATE TABLE transactions (
-  id SERIAL PRIMARY KEY,
-  src_bank_account VARCHAR(255) NOT NULL,
-  dst_bank_account VARCHAR(255) NOT NULL,
-  amount DECIMAL(10, 2) NOT NULL,
-  direction VARCHAR(10) NOT NULL,
-  status VARCHAR(10) NOT NULL
-);
