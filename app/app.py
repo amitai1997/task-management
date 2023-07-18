1e5513b66bc07a62f0800a85cc843dc4e2a507ad
amitai salmon <amitai1997@gmail.com>
2023-07-18 11:53:29 +0300

    last working version

--- app/app.py
+++ app/app.py
@@ -35,4 +35,4 @@ api.add_resource(IndexResource, '/')
 api.add_resource(RoutesResource, '/routes')
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
